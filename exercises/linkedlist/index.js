135c09233512d9a1181b55b243e8cda5230b7799
Maria Ma <marie0901@gmail.com>
2019-01-11 22:15:48 +0200

    from asus

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,56 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor (data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let s = 0;
+    let node = this.head;
+    while (node) {
+      s += 1;
+      node = node.next;
+    }
+
+    return s;
+  }
+
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let last = this.head;
+
+    while (last.next) {
+      last = last.next
+    }
+    return last;
+  }
+
+  clear(){
+    this.head = null;
+  }
+
+  removeFirst(){
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else this.head = null;
+  }
+}
 
 module.exports = { Node, LinkedList };
